3b676249f3bf941da671204b91d35434095fdcef
saurabhsinghss241 <103527528+saurabhsinghss241@users.noreply.github.com>
2023-12-05 21:13:19 +0530

    Updated RedisCaching.cs
    Added Get and Set method in RedisCaching.cs

--- CachingService/RedisCaching.cs
+++ CachingService/RedisCaching.cs
@@ -3,19 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using System;
 
 namespace CachingService
 {
-    internal class RedisCaching : ICaching
+    public class RedisCaching : ICaching
     {
+        private readonly ConnectionMultiplexer _connection;
+        private readonly IConfiguration _configuration;
+        
+        public RedisCaching(IConfiguration configuration){
+            _configuration = configuration;
+            var connectionString = _configuration.GetConnectionString("RedisCache");
+            _connection = ConnectionMultiplexer.Connect(connectionString);
+        }
+        
         public Task<string> GetCache<T>(string key)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var database = _connection.GetDatabase();
+                return database.StringGet(key);
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (log or throw)
+                Console.WriteLine($"Error getting value from Redis Cache: {ex.Message}");
+                return null;
+            }
         }
 
         public Task<bool> SaveCache<T>(string key, T data, TimeSpan expiry)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var database = _connection.GetDatabase();
+                return database.StringSet(key, value);
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (log or throw)
+                Console.WriteLine($"Error setting value in Redis Cache: {ex.Message}");
+                return false;
+            }
         }
     }
 }
